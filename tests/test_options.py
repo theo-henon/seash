e53645660836e17500ec22a1e8b46f5f6922e4a9
LeLapinouAlpha <t-henon@hotmail.fr>
2024-04-07 10:37:42 +0200

    tests: use copy of EXE & REF arrays

--- tests/test_options.py
+++ tests/test_options.py
@@ -25,13 +25,10 @@ def test_no_options_with_file():
 
 
 def test_c_option_with_file():
-    cmd = config.EXE + ["-c", "echo", "whatever.sh"]
-    cmd_ref = config.REF + ["-c", "echo", "whatever.sh"]
-    exe = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    ref = subprocess.Popen(cmd_ref, stdout=subprocess.PIPE)
+    cmd = config.EXE.copy() + ["-c", "echo", "whatever.sh"]
+    exe = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     exe_status = exe.wait()
-    ref_status = ref.wait()
-    assert exe_status == ref_status and exe.stdout.read().decode("utf-8") == ref.stdout.read().decode("utf-8")
+    assert exe_status == 1 and exe.stdout.read().decode("utf-8") == "" and exe.stderr.read().decode("utf-8") != ""
 
 
 def test_pretty_print_option_with_file():
@@ -40,12 +37,12 @@ def test_pretty_print_option_with_file():
 
 
 def test_pretty_print_c_options():
-    cmd = config.EXE_PRETTY_PRINT + ["-c", "echo"]
+    cmd = config.EXE_PRETTY_PRINT.copy() + ["-c", "echo"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     assert process.wait() == 0 and process.stdout.read().decode("utf-8") == "echo"
 
 
 def test_c_pretty_print_options():
-    cmd = config.EXE + ["-c", "echo", "--pretty-print"]
+    cmd = config.EXE.copy() + ["-c", "echo", "--pretty-print"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     assert process.wait() == 0 and process.stdout.read().decode("utf-8") == "echo"
