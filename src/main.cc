e2136ba2052b1abe84c0645de7fe17810dd00b03
LeLapinouAlpha <t-henon@hotmail.fr>
2024-04-07 10:14:50 +0200

    refactor(main): parse options by hand

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
-#include <boost/program_options.hpp>
+#include <fstream>
 #include <iostream>
+#include <optional>
 #include <sstream>
 
 #include "lexer/lexer.hh"
@@ -7,59 +8,74 @@
 #include "visitor/execution_visitor.hh"
 #include "visitor/print_visitor.hh"
 
-namespace po = boost::program_options;
-
 int main(int argc, char* argv[])
 {
-    po::options_description options_desc("Options");
-    options_desc.add_options()("help", "Produce help message")(
-        "c,c", po::value<std::string>(),
-        "Execute script from string passed as argument")(
-        "pretty-print", "Enable pretty pretting of the AST");
+    bool pretty_print = false;
+    std::optional<std::string> filename;
+    std::optional<std::string> script;
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, options_desc), vm);
-    po::notify(vm);
-
-    if (vm.count("help"))
+    // Parse options
+    for (int i = 1; i < argc; i++)
     {
-        std::cout << options_desc << std::endl;
-        return 0;
+        if (std::string(argv[i]) == "-c")
+        {
+            if (++i < argc)
+                script = argv[i];
+            else
+            {
+                std::cerr << "Missing string argument for '-c' option"
+                          << std::endl;
+                return 1;
+            }
+        }
+        else if (std::string(argv[i]) == "--pretty-print")
+            pretty_print = true;
+        else
+            filename = argv[i];
     }
 
-    if (vm.count("c"))
+    if (script && filename)
     {
-        std::istringstream input(vm["c"].as<std::string>());
+        std::cerr << "Unexpected argument: '" << *filename << '\''
+                  << std::endl;
+        return 1;
+    }
+
+    std::unique_ptr<ast::SimpleCmd> simple_cmd;
+    if (script)
+    {
+        std::istringstream input(script.value());
         lexer::Lexer lexer(input);
         parser::Parser parser(lexer);
-        auto simple_cmd = parser.parse_simple_cmd();
-
-        if (vm.count("pretty-print"))
-        {
-            visitor::PrintVisitor print_visitor;
-            return simple_cmd->accept(&print_visitor);
-        }
-        else
-        {
-            visitor::ExecutionVisitor execution_visitor;
-            return simple_cmd->accept(&execution_visitor);
-        }
+        simple_cmd = parser.parse_simple_cmd();
+    }
+    else if (filename)
+    {
+        std::ifstream input(*filename);
+        if (!input.is_open())
+            std::perror(filename->c_str());
+        lexer::Lexer lexer(input);
+        parser::Parser parser(lexer);
+        simple_cmd = parser.parse_simple_cmd();
     }
     else
     {
         lexer::Lexer lexer(std::cin);
         parser::Parser parser(lexer);
-        auto simple_cmd = parser.parse_simple_cmd();
+        simple_cmd = parser.parse_simple_cmd();
+    }
 
-        if (vm.count("pretty-print"))
-        {
-            visitor::PrintVisitor print_visitor;
-            return simple_cmd->accept(&print_visitor);
-        }
-        else
-        {
-            visitor::ExecutionVisitor execution_visitor;
-            return simple_cmd->accept(&execution_visitor);
-        }
+    if (simple_cmd == nullptr)
+        return 1;
+
+    if (pretty_print)
+    {
+        visitor::PrintVisitor print_visitor;
+        return simple_cmd->accept(&print_visitor);
+    }
+    else
+    {
+        visitor::ExecutionVisitor execution_visitor;
+        return simple_cmd->accept(&execution_visitor);
     }
 }
